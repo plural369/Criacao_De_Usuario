b45e56e6c81746194fc69d948176af76ab68a998
plural369 <57465395+plural369@users.noreply.github.com>
2021-06-22 16:58:02 -0300

    Completo

---
+++ CriacaoDeUsuarios/CriacaoDeUsuarios/Context/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CriacaoDeUsuarios.Context
+{
+    public class Contexto : IdentityDbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        { }
+    }
+}
---
+++ CriacaoDeUsuarios/CriacaoDeUsuarios/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CriacaoDeUsuarios.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CriacaoDeUsuarios.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Cadastrar(RegisterViewModel cadastro)
+        {
+            if (ModelState.IsValid)
+            {
+                var usuarios = new IdentityUser
+                {
+                    UserName = cadastro.Email,
+                    Email = cadastro.Email
+                };
+                var confirma = await userManager.CreateAsync(usuarios, cadastro.Password);
+                return RedirectToAction(nameof(Cadastrar));
+            }
+            return View(cadastro);
+        }
+
+    }
+}
